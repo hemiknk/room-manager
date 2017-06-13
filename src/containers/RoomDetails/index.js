50b1ccd3ce65c8b4c0620c62f93cee2b7948ef43
Artem Tarasov <artem.tarasov@nixsolutions.com>
2017-06-13 20:04:36 +0300

    add edit room form

--- src/containers/RoomDetails/index.js
+++ src/containers/RoomDetails/index.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 class RoomDetails extends Component {
     render() {
 
-        return <div className='col-md-offset-3'>
+        return <div>
+            <form>
+                <div className='row'>
+                    <div className='col-md-6 form-group'>
+                        <p><h3><span className='label label-info'>ROOM DETAILS</span></h3></p>
+                    </div>
+                    <div className='com-md-6 form-group'>
+                        <button className='btn btn-danger'>Delete room</button>
+                    </div>
+                </div>
+                <div className='form-group row'>
+                    <label className='col-sm-2 col-form-label'>Name:</label>
+                    <div className='col-sm-8'>
+                        <input type='text' className='form-control' placeholder='Room name'/>
+                    </div>
+                </div>
+                <div className='form-group row'>
+                    <label className='col-sm-2 col-form-label'>Description:</label>
+                    <div className='col-sm-8'>
+                        <textarea className='form-control' rows='10' cols='10' placeholder='Description'/>
+                    </div>
+                </div>
+                <botton>Go back</botton>
+                <botton>Save</botton>
+
+            </form>
             Детали комнаты с ид={this.props.params.roomId}
             {this.props.children}
         </div>
@@ -15,10 +40,10 @@ const mapStateToProps = (state) => ({
     st: state
 });
 /*
-function mapDispatchToProps(dispatch) {
-    return {
-        addRoom: bindActionCreators(addRoom, dispatch),
-        filterByRoomName: bindActionCreators(filterByRoomName, dispatch)
-    }
-}*/
+ function mapDispatchToProps(dispatch) {
+ return {
+ addRoom: bindActionCreators(addRoom, dispatch),
+ filterByRoomName: bindActionCreators(filterByRoomName, dispatch)
+ }
+ }*/
 export default connect(mapStateToProps)(RoomDetails)
